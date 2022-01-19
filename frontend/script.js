7ab660dc015a3cd24e7096b1debb2144ff4ead6a
porneczy <porneczy@gmail.com>
2022-01-19 18:31:25 +0100

    add a data array script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,55 @@
+/* const data = [
+    "Gergely Kiss",
+    "Krisztián Bui",
+    "Buczkó Balázs",
+    "András Varga",
+    "Borbála Teréz Kovács"
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favoriteFood: "csirkerizs"
+    },
+    {
+        name: "Krisztián Bui",
+        favoriteFood: "banán"
+    },
+    {
+        name: "Buczkó Balázs",
+        favoriteFood: "kolbászos rájás pizza"
+    },
+    {
+        name: "András Varga",
+        favoriteFood: "brassói"
+    },
+    {
+        name: "Borbála Teréz Kovács",
+        favoriteFood: "avokádó"
+    },
+    {
+        name: "Benett Viszokai",
+        favoriteFood: "alma"
+    },
+    {
+        name: "Pörneczi Krisztián",
+        favoriteFood: "almáspite"
+    },
+]
+
 function loadEvent() {
     console.log("Az oldal betöltődött")
 
-    const list = document.querySelectorAll("section")
+    const root = document.querySelector("#root")
 
-    
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend", `
-        <p>text</p>
+    for (const frontendStudent of data) {
+        root.insertAdjacentHTML("beforeend", `
+        <section>
+            <h1>${frontendStudent.name}</h1>
+            <p>${frontendStudent.favoriteFood}</p>
+        </section>
         `)
-        //elem.querySelector("p").style.fontFamily = `'Poppins', sans-serif`;
-        //elem.querySelector("h1").outerHTML = ""
     }
 }
 window.addEventListener("load", loadEvent)
